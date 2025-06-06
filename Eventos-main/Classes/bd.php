888bf150135090f395c938639f607ec1ec5338c1
Melissa Beatriz Vieira <160934310+melissabvieira@users.noreply.github.com>
2025-06-05 22:05:12 -0300

    Update bd.php

--- Eventos-main/Classes/bd.php
+++ Eventos-main/Classes/bd.php
@@ -3,8 +3,9 @@ require 'vendor/autoload.php';
 use MongoDB\Client;
 
 try {
-    $mongoClient = new Client("mongodb://localhost:27017"); 
-    $db = $mongoClient->selectDatabase('eventos'); 
+    $mongoClient = new Client("mongodb://localhost:27017");
+    $db = $mongoClient->selectDatabase('eventos');
+    $collection = $db->selectCollection('eventos');
 } catch (Exception $e) {
     die("Erro ao conectar no MongoDB: " . $e->getMessage());
 }
